7b1a128ddbf4b4cbb3f331393a2832b39529ea27
Dilshan221 <dunith0773239462@gmail.com>
2026-05-30 17:13:10 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.json({ message: "API is running" });
+    res.send("API is running ");
+
 });
 
 module.exports = app;
